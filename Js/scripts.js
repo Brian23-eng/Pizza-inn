5c5b93c98eaf38482168d496a9f6fb6b6b564984
brian23-eng <b.odhiambo.bo@gmail.com>
2019-08-30 22:30:13 +0300

    add a toggle to images and style descriptive paragraphs in index.html

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,7 +1,29 @@
+//user interface one
 $(document).ready(function() {
 
     $(".picha").click(function() {
 
         $(".jaza").fadeToggle("slow");
+    });
+
+    $(".eve1").hover(function() {
+        $(".para").slideToggle(1000);
+        $("#img1").slideToggle(1000);
+        $(".para").show();
+    });
+    $(".eve2").hover(function() {
+        $(".para1").slideToggle(1000);
+        $("#img2").slideToggle(1000);
+        $(".para1").show();
+    });
+    $(".eve3").hover(function() {
+        $(".para2").slideToggle(1000);
+        $("#img3").slideToggle(1000);
+        $(".para2").show();
+    });
+    $(".eve4").hover(function() {
+        $(".para3").slideToggle(1000);
+        $("#img4").slideToggle(1000);
+        $(".para3").show();
     });
 })
